8002df26b94829021ebbba897a2ac77d22b4e181
Valentin Haenel <valentin@haenel.co>
2016-07-17 20:06:47 +0200

    refactor file linking

--- src/main/python/yadoma/main.py
+++ src/main/python/yadoma/main.py
@@ -39,37 +39,40 @@ def get_subcommand(arguments):
         raise CMDLineExit
 
 
+def link(file_, base_dir, target_dir):
+    try:
+        src = os.path.abspath(os.path.join(base_dir, file_['src']))
+    except KeyError:
+        raise ConfigError("Missing 'src' entry")
+    try:
+        dest = os.path.join(target_dir, file_['dest'])
+    except KeyError:
+        dest = os.path.join(target_dir, file_['src'])
+    debug("Will try to symlink '{0}' to '{1}'...".format(src, dest))
+    try:
+        os.symlink(src, dest)
+
+    except OSError as ose:
+        if ose.errno == 17:
+            debug("symlink exists")
+        else:
+            raise
+    else:
+        debug("successful")
+
+
 def main():
     arguments = docopt(__doc__)
     global DEBUG
     if arguments['--debug']:
         DEBUG = True
     config_path = arguments['<config>']
-    dir_name = os.path.dirname(arguments['<config>'])
-    home_dir = os.environ['HOME']
+    base_dir = os.path.dirname(arguments['<config>'])
+    target_dir = os.environ['HOME']
     loaded_config = yaml.load(open(config_path).read())
     debug(loaded_config)
     subcommand = get_subcommand(arguments)
     if subcommand == LINK:
         for program, program_config in loaded_config.items():
             for file_ in program_config['files']:
-                try:
-                    src = os.path.abspath(os.path.join(dir_name, file_['src']))
-                except KeyError:
-                    raise ConfigError("Missing 'src' entry")
-                try:
-                    dest = os.path.join(home_dir, file_['dest'])
-                except KeyError:
-                    dest = os.path.join(home_dir, file_['src'])
-                debug("Will try to symlink '{0}' to '{1}'...".format(src, dest))
-                try:
-                    os.symlink(src, dest)
-
-                except OSError as ose:
-                    if ose.errno == 17:
-                        debug("symlink exists")
-                    else:
-                        raise
-                else:
-                    debug("successful")
-
+                link(file_, base_dir, target_dir)
